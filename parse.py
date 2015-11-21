4258440d1fab94421e1c1c3a2c752e74831fd565
Zachary Rump <nihilanth41@gmail.com>
2015-11-21 15:31:48 -0600

    Add parse.py, read input file char-by-char and store them as cartesian coords

---
+++ parse.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import sys
+
+# Input arg is map file
+if len(sys.argv) != 2:
+	print "Usage is %s <input file>\n" % ( argv[0] )
+
+class Node:
+	def __init__(self, xpos, ypos):
+		self.x = xpos
+		self.y = ypos
+
+# Robot 1: S -> E
+# Robot 2: F -> L
+
+# Print room.txt to stdout line by line
+nodeArr = []
+with open(sys.argv[1]) as f:
+	lines = f.readlines()
+	y = len(lines)
+	for l in lines:
+		x = 0
+		y -= 1
+		if y != 0:
+			print y,
+		
+		for word in l:
+			for ch in word:
+				x += 1
+				nodeArr.append(Node(x, y))
+				i = len(nodeArr)-1
+				print ch,
+				#print "[%d,%d]" % (nodeArr[i].x, nodeArr[i].y),
+		#print ""
+				
+
+
+			
+
